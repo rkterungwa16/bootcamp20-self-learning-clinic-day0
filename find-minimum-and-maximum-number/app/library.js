2ca5c5d6940998585cfd77595e077b9228e5ade2
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-03-11 10:23:47 +0100

    implemented and tested fizzbuzz

---
+++ find-minimum-and-maximum-number/app/library.js
@@ -0,0 +1,28 @@
+module.exports = {
+ 
+  findMinMax: function (arr) {
+  	var minMaxArray = [];
+  	if (arr.length === 0) {
+  		return [];
+  	}
+
+  	var maxNumber = Number.MIN_VALUE;
+	var minNumber = Number.MAX_VALUE;
+	for (var i=0; i<arr.length; i++) {
+		if (arr[i] > maxNumber) {
+			maxNumber = arr[i];
+		}
+		if (arr[i] < minNumber) {
+			minNumber = arr[i];
+		}
+	}
+	if (maxNumber === minNumber) {
+		minMaxArray.push(minNumber);
+		return minMaxArray;
+	}
+
+ 	minMaxArray.push(minNumber);
+ 	minMaxArray.push(maxNumber);
+	return minMaxArray;
+  }
+}
